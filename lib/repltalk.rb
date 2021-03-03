51d81eadbabfa0b4840ec24ec6348a93bf3f2f6d
Coding Cactus <CodingCactus.cc@gmail.com>
2021-03-03 17:14:59 +0000

    completed User#initialze and made this into a gem

--- lib/repltalk.rb
+++ lib/repltalk.rb
@@ -2,8 +2,38 @@ require "http"
 require "json"
 require_relative "queries"
 
+class Role
+	attr_reader :name, :key, :tagline
+
+	def initialize(role)
+		@name = role["name"]
+		@key = role["key"]
+		@tagline = role["tagline"]
+	end
+
+	def to_s
+		@name
+	end
+end
+
+class Language
+	attr_reader :id, :key, :name, :tagline, :icon
+
+	def initialize(lang)
+		@id = lang["id"]
+		@key = lang["key"]
+		@name = lang["displayName"]
+		@tagline = lang["tagline"]
+		@icon = lang["icon"]
+	end
+
+	def to_s
+		@id
+	end
+end
+
 class User
-	attr_reader :id, :username, :name, :pfp, :bio, :cycles, :is_hacker, :roles
+	attr_reader :id, :username, :name, :pfp, :bio, :cycles, :is_hacker, :roles, :languages
 
 	def initialize(user)
 		@id = user["id"]
@@ -13,12 +43,17 @@ class User
 		@bio = user["bio"]
 		@cycles = user["karma"]
 		@is_hacker = user["isHacker"]
-		@roles = user["roles"] # will do something proper with them eventually
+		@roles = user["roles"].map { |role| Role.new(role) }
+		@languages = user["languages"].map { |lang| Language.new(lang) }
+	end
+
+	def to_s
+		@username
 	end
 end
 
 class Client
-	attr_accessor :sid
+	attr_writer :sid
 
 	def initialize(sid=nil)
 		@sid = sid
---
+++ repltalk.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+	s.name        = 'repltalk'
+	s.version     = '0.0.1'
+	s.summary     = "A ruby client for the repltalk api"
+	s.description = "With the repltalk gem, you can easily interect with the repltalk graphql api."
+	s.authors     = ["CodingCactus"]
+	s.email       = 'codingcactus.cc@gmail.com'
+	s.files       = ["lib/repltalk.rb", "lib/queries.rb"]
+	s.metadata    = { "source_code_uri" => "https://github.com/Codin-Cactus/repltalk" }
+end
