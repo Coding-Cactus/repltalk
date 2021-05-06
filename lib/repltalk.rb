dd71c239660fa0d4adde16c786296df64638b7c2
Coding Cactus <CodingCactus.cc@gmail.com>
2021-05-06 07:38:17 +0000

    finally forced to move to replit.com :(

--- lib/repltalk.rb
+++ lib/repltalk.rb
@@ -3,6 +3,6 @@ require_relative "repltalk/graphql"
 require_relative "repltalk/structures"
 
 module ReplTalk
-	$BASE_URL = "https://repl.it"
-	VERSION = "3.1.0"
+	$BASE_URL = "https://replit.com"
+	VERSION = "3.2.0"
 end
